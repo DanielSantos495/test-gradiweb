b1a827955c90fb5c0bbb20de4e09acedc2630696
Daniel Santos <dsantos0495@gmail.com>
2021-02-01 18:14:13 -0500

    Configuracion dev OK

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+   <h1>Comenzamos prueba</h1>,
+   document.getElementById('app')
+)
---
+++ webpack.dev.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { HotModuleReplacementPlugin } = require('webpack')
+
+module.exports = {
+   entry: path.resolve(__dirname, 'src/index.js'),
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].js'
+   },
+   resolve: {
+      extensions: ['.js', '.jsx']
+   },
+   devServer: {
+      port: 9000,
+      hot: true,
+      open: true
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js|jsx$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+         },
+         {
+            test: /\.css|scss$/,
+            use: [
+               'style-loader',
+               'css-loader',
+               'sass-loader'
+            ]
+         },
+         {
+            test: /\.png|jpe?g/,
+            use: [
+               {
+                  loader: 'file-loader'
+               }
+            ]
+         }
+      ]
+   },
+   plugins: [
+      new HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+         template: path.resolve(__dirname, 'public/index.html'),
+         filename: 'index.html'
+      })
+   ]
+}
